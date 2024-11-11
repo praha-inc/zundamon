c488ae8a6c642ab441443508adbeb91d5d1263b2
Karibash <y.karibayashi@gmail.com>
2024-11-11 17:33:29 +0900

    chore: Change organization name

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import { common } from '@agaroot/eslint-config-common';
-import { define } from '@agaroot/eslint-config-definer';
-import { javascript } from '@agaroot/eslint-config-javascript';
-import { style } from '@agaroot/eslint-config-style';
-import { typescript } from '@agaroot/eslint-config-typescript';
+import { common } from '@praha/eslint-config-common';
+import { define } from '@praha/eslint-config-definer';
+import { javascript } from '@praha/eslint-config-javascript';
+import { style } from '@praha/eslint-config-style';
+import { typescript } from '@praha/eslint-config-typescript';
 
 const config = define([
   common,
